bca57f5f104435a7adca949e8d081f02b6b2b516
Hemant H Kumar <hemant.hari.kumar@gmail.com>
2021-07-03 04:02:34 +0530

    Re-organise code into client and server folders

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//import routes
+require("./routes/predict.js");
+
+const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+// const path = require("path");
+
+// Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
